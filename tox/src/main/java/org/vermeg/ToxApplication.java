9f0be674b4ba3575883c6feb38e2318432bdd439
workinginit <workinginitgit@gmail.com>
2018-05-01 10:33:35 +0100

    fff

---
+++ tox/src/main/java/org/vermeg/ToxApplication.java
@@ -0,0 +1,12 @@
+package org.vermeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToxApplication.class, args);
+	}
+}
